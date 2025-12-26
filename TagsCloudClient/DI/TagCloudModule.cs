8b359517546d324a69daf8698a06f805eb0b597c
Matvey <rusinovmatvei@gmail.com>
2025-12-26 23:49:20 +0500

    completed DI in client

--- TagsCloudClient/DI/TagCloudModule.cs
+++ TagsCloudClient/DI/TagCloudModule.cs
@@ -10,22 +10,22 @@ using TagsCloudCore.Text.Implementations.WordsPreprocessing;
 using TagsCloudCore.Text.Implementations.WordsPreprocessing.Filters;
 using TagsCloudCore.Text.Implementations.WordsProviding;
 
-namespace TagsCloudClient;
+namespace TagsCloudClient.DI;
 
-public class TagsCloudModule(
-    string filePath,
-    int minFontSize = 30,
-    int maxFontSize = 200,
-    int centerX = 0,
-    int centerY = 0) : Module
+public class TagsCloudModule(string filePath, int minFontSize, IEnumerable<string>? stopWords = null) : Module
 {
-    private readonly Point center = new(centerX, centerY);
+    private static readonly Point Center = new(0, 0);
 
-    private static readonly string[] parameterValue =
+    private static readonly string[] DefaultStopWords =
     [
         "и", "в", "во", "на", "с", "со",
         "а", "но", "что", "как", "к", "до",
-        "по", "из", "у", "или", "же"
+        "по", "из", "у", "или", "же", "он", 
+        "она", "они", "я", "вы", "ты", "чем", 
+        "от", "ее", "её", "за", "все", "всё",
+        "был", "его", "еще", "ещё", "было",
+        "была", "лишь", "не", "под", "над", "нем",
+        "нём", "ней"
     ];
 
     protected override void Load(ContainerBuilder builder)
@@ -50,7 +50,7 @@ public class TagsCloudModule(
         builder.RegisterType<StopWordsFilter>()
             .As<IWordsFilter>()
             .SingleInstance()
-            .WithParameter("stopWords", parameterValue);
+            .WithParameter("stopWords", (stopWords ?? DefaultStopWords).ToArray());
 
         builder.RegisterType<WordsPreprocessor>()
             .As<IWordsPreprocessor>()
@@ -63,8 +63,7 @@ public class TagsCloudModule(
         builder.RegisterType<WordsRelativeFontSizer>()
             .As<IWordsFontSizer>()
             .SingleInstance()
-            .WithParameter("minFontSize", minFontSize)
-            .WithParameter("maxFontSize", maxFontSize);
+            .WithParameter("minFontSize", minFontSize);
 
         builder.RegisterType<ToCenterTightener>()
             .As<IRectangleTightener>()
@@ -73,7 +72,7 @@ public class TagsCloudModule(
         builder.RegisterType<CircularCloudLayouter>()
             .As<ICloudLayouter>()
             .SingleInstance()
-            .WithParameter("center", center);
+            .WithParameter("center", Center);
 
         builder.RegisterType<WordsLayoutGenerator>()
             .As<IWordsLayoutGenerator>()
